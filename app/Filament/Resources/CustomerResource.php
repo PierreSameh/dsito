9a244bd40555d0989c9ed4ee80a529142d739a43
PierreSameh <captainpirzon@gmail.com>
2024-11-30 00:00:42 +0200

    #P update delivery men icon

--- app/Filament/Resources/CustomerResource.php
+++ app/Filament/Resources/CustomerResource.php
@@ -19,7 +19,7 @@ class CustomerResource extends Resource
 {
     protected static ?string $model = Customer::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static ?string $navigationIcon = 'heroicon-o-truck';
 
     public static function getLabel(): ?string
     {
